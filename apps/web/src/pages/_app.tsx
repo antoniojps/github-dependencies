fbfd47c7a5ee211733862b4b69328f1172702f7a
antoniojps <antonio.santos@mindera.com>
2021-04-04 00:02:05 +0100

    feat(web): add react-query

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -2,14 +2,19 @@ import { ReactElement } from 'react';
 import { AppProps } from 'next/app';
 import { Theme } from '@github-graphs/components';
 import { Provider as NextAuthProvider } from 'next-auth/client';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 import '@github-graphs/components/containers/Theme/Theme.scss';
+
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps): ReactElement {
   return (
     <Theme>
       <NextAuthProvider session={pageProps.session}>
-        <Component {...pageProps} />
+        <QueryClientProvider client={queryClient}>
+          <Component {...pageProps} />
+        </QueryClientProvider>
       </NextAuthProvider>
     </Theme>
   );
