56d9ac3df8b4185e5cb44dfe388cda498e5ee54c
antoniojps <antonio.santos@mindera.com>
2021-04-04 00:09:09 +0100

    feat(web): basic dependencies fetch

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,16 +1,42 @@
-import React from 'react';
+import React, { useEffect, useMemo } from 'react';
 import { Dependencies, Layout } from '@github-graphs/components';
 import { useSession, signOut, signIn } from 'next-auth/client';
+import { useQuery } from 'react-query';
+import { ParserResult } from '@github-graphs/types';
+import { filterTopRelatedDependencies } from '@github-graphs/parser-dependencies/filters';
 
 export function Index() {
   const [session, loading] = useSession();
+  const { isLoading, error, data, refetch } = useQuery<ParserResult>(
+    'dependencies',
+    () => fetch('/api/dependencies').then((res) => res.json()),
+    {
+      enabled: false,
+      refetchOnWindowFocus: false,
+    }
+  );
+
+  useEffect(() => {
+    if (session) {
+      refetch();
+    }
+  }, [session]);
+
+  const dataParsed = useMemo(() => {
+    if (!data) return [];
+    const npm = data.find((d) => d.packageManager === 'npm');
+    return filterTopRelatedDependencies(npm.data).slice(0, 10).reverse();
+  }, [data]);
+
   return (
     <Layout nav={{ user: session?.user, handleSignOut: signOut, handleSignIn: signIn }}>
       <Dependencies
         user={session?.user}
         isLoadingUser={loading}
         handleSignIn={signIn}
-        isLoading={false}
+        isLoading={isLoading}
+        isError={Boolean(error)}
+        data={dataParsed}
       />
     </Layout>
   );
