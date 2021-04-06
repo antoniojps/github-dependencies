dd4566c3a53c814e1de26f71fbf5cec23b85f2c8
antoniojps <antonio.santos@mindera.com>
2021-04-06 20:52:29 +0100

    feat(web): seo

--- apps/web/src/containers/Seo/Seo.data.ts
+++ apps/web/src/containers/Seo/Seo.data.ts
@@ -1,15 +1,12 @@
 export const SITE_METADATA = {
-  url:
-    process.env.DEPLOY_ENV === 'production'
-      ? 'https://github.antoniosantos.me'
-      : 'https://github-graphs.vercel.app',
-  title: 'Github Personal Statistics Graphs',
+  url: 'https://dependencies.vercel.app',
+  title: 'Github Dependencies Chart Generator - Check Your Top Dependencies',
   description:
-    'Generate your github usage graphs for your top dependencies and programming languages.',
+    'Understand what npm and composer dependencies you use the most on your github projects.',
   keywords: [
-    'github year wrapped',
     'github  dependency usage graph ',
-    'github programming languages usage graph',
+    'github dependendencies chart',
+    'github dependencies chart generator',
   ],
   image: { src: '/share.jpg', width: 1686, height: 882 },
 };
